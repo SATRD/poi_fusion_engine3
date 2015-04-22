031952bb2b6da230fe9ea6d26c09cfbd05bd7e65
BuildTools <unconfigured@null.spigotmc.org>
2015-04-22 13:06:07 +0200

    Change from tomcat8 to tomcat7 in sql files.

--- ficontent2/docker/install.sh
+++ ficontent2/docker/install.sh
@@ -35,6 +35,9 @@ cd poi_fusion_engine3/release/frontend
 cp fic2_fe_v3_frontend.war /var/lib/tomcat7/webapps/
 # Sleep for a while so that Tomcat can deploy the WAR file
 sleep 10
+# Change from tomcat8 to 7
+sed -i 's/tomcat8/tomcat7/' /var/lib/tomcat7/webapps/fic2_fe_v3_frontend/config/dbScripts/ocd_valencia_demo.sql
+sed -i 's/tomcat8/tomcat7/' /var/lib/tomcat7/webapps/fic2_fe_v3_frontend/config/dbScripts/tenerife_valencia_demo.sql
 # Initialize the app with demo data (valencia_demo and tenerife_demo). This may take a while
 wget http://localhost:8080/fic2_fe_v3_frontend/init.jsp
 
