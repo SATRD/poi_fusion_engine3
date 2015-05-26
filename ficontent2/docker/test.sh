8bb2c575b149a0a240c49444e133be6afdf62477
Benjamin Molina <benmomo@users.noreply.github.com>
2015-05-26 12:42:19 +0200

    Update test.sh
    changed wget parameter location

--- ficontent2/docker/test.sh
+++ ficontent2/docker/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget http://$1:$2/fic2_fe_v3_frontend/citysdk/categories/search?list=poi&ocdName=valencia_demo -q -O down.json >/dev/null
-wget http://$1:$2/fic2_fe_v3_frontend/citysdk/down_expexted.json -q -O down_ex.json >/dev/null
+wget -q -O down.json http://$1:$2/fic2_fe_v3_frontend/citysdk/categories/search?list=poi&ocdName=valencia_demo  >/dev/null
+wget -q -O down_ex.json http://$1:$2/fic2_fe_v3_frontend/citysdk/down_expexted.json  >/dev/null
 if diff ./down_ex.json ./down.json >/dev/null ; then
   echo "Test passed OK"
   echo 0
